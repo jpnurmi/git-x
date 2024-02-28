da325240aac36c2bbce69aa536db149a5788b099
J-P Nurmi <jpnurmi@gmail.com>
2024-02-28 09:27:39 +0100

    git-remote-dup: add --fetch

--- git-x.sh
+++ git-x.sh
@@ -4,7 +4,7 @@ export PATH="$(dirname "${BASH_SOURCE[0]:-$0}"):$PATH"
 
 _complete_git_remote_dup() {
     local cur=${COMP_WORDS[COMP_CWORD]}
-    local opts="-h --help --https --ssh"
+    local opts="-h --help --fetch --https --ssh"
     local remotes="$(git remote)"
     local dup="$GIT_REMOTE_DUP"
     COMPREPLY=($(compgen -W "$opts $remotes $dup" -- $cur))
